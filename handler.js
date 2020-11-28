c88746d49500c77190d3b153316a608be434e875
JonahAcquah <jonah.acquah@yum.com>
2020-11-28 18:53:55 +0000

    Start machine from http endpoint

--- handler.js
+++ handler.js
@@ -13,9 +13,6 @@ module.exports.hello = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.hello2 = async event => {
@@ -31,24 +28,4 @@ module.exports.hello2 = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
-module.exports.engineStart = async event => {
-  const aws = require('aws-sdk')
-  var params = {
-    stateMachineArn: process.env.statemachine_arn,
-    input: JSON.stringify({})
-  }
-
-  const stepfunctions = new aws.StepFunctions()
-  stepfunctions.startExecution(params, function (err, data) {
-    if (err) {
-      console.log('err while executing step function')
-    } else {
-      console.log('started execution of step function')
-    }
-  })
 };
